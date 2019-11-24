b929096d625948320955ffe9c4e15b82bc4e0ec3
Luke <deandrevidal@aol.com>
2019-11-24 12:04:58 -0800

    added my_dogs.py

--- dog.py
+++ dog.py
@@ -1,9 +1,9 @@
 # dog.py
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
         print("dog initialized!")
 
-
-# instantiation call that creates a Dog object:
-Dog("Rex")
+    def bark(self):
+        print("Woof!")

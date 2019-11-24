e3f3355e30381bc6b5ebffeb6e3a0a904d06a1b3
Luke <deandrevidal@aol.com>
2019-11-24 12:17:18 -0800

    paghe 1 stretch complete

--- dog.py
+++ dog.py
@@ -3,7 +3,12 @@ class Dog:
     def __init__(self, name, breed):
         self.name = name
         self.breed = breed
-        print("dog initialized!")
 
     def bark(self):
-        print("Woof!")
+        print(f'{self.name} barks!')
+
+    def sit(self):
+        print(f'{self.name} sits!')
+
+    def roll_over(self):
+        print(f'{self.name} rolled over!')
--- my_dogs.py
+++ my_dogs.py
@@ -3,3 +3,12 @@ from dog import Dog  # we need to specify exactly what we want
 
 my_dog = Dog("Rex", "SuperDog")
 my_dog.bark()
+
+my_other_dog = Dog("Annie", "SuperDog")
+my_other_dog.bark()
+
+another_dog = Dog("Cane", "Shadow Hound")
+another_dog.sit()
+
+hound = Dog("Mordicai", "Mordicainian's Faithful Hound")
+hound.roll_over()
